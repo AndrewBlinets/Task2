c44f822d20056f1acdc14594fef952ddfe206e11
Andrew <andreiblinets@gmail.com>
2017-03-30 11:37:47 +0300

    create connector DB

--- dao/src/main/java/by/andreiblinets/database/dao/CreditCardDAOImp.java
+++ dao/src/main/java/by/andreiblinets/database/dao/CreditCardDAOImp.java
@@ -5,7 +5,7 @@ import by.andreiblinets.entity.CreditCard;
 import java.util.List;
 
 public class CreditCardDAOImp implements CreditCardDAO {
-    public List<CreditCard> getUserCard(long idUser) {
+    public List<CreditCard> getUserCard(long idUser){
         return null;
     }
 }
--- dao/src/main/java/by/andreiblinets/database/dao/PaymentDAOImp.java
+++ dao/src/main/java/by/andreiblinets/database/dao/PaymentDAOImp.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class PaymentDAOImp implements PaymentDAO {
     public List<Payment> getAllPayment() {
+
         return null;
     }
 }
